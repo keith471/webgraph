a7bbc0843b2fea400bc14f189ed21d376deaa5ca
Keith Strickling <keithstrickling@gmail.com>
2017-09-03 16:18:23 -0700

    added content.js

--- js/eventpage.js
+++ js/eventpage.js
@@ -4,6 +4,10 @@
 
 console.log("this thing has loaded");
 
+chrome.commands.getAll(function(commands){
+    console.log(commands)
+});
+
 chrome.commands.onCommand.addListener(function(command) {
     console.log('Command:', command);
 });
